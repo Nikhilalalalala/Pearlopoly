7b1dd387d7d4b64655d7fea987acc78934b64ab5
Nikhilalalalala <uvnikhi@gmail.com>
2020-06-07 12:25:04 +0800

    adding Loading Screen, but nothing in that yet

---
+++ Screens/Loading/LoadingScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import firebase from "firebase";
+
+class LoadingScreen extends Component {
+
+    componentDidMount() {
+        this.checkifLoggedIn();
+    }
+
+    checkifLoggedIn = () => {
+        firebase.auth().onAuthStateChanged( (user) => {
+            if (user) {
+              console.log("hello")
+                this.props.navigation.navigate('OverviewScreen')
+            } else {
+                this.props.navigation.navigate('LoginScreen')
+            }
+        })
+    }
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Wait ah</Text>
+      </View>
+    );
+  }
+}
+
+export default LoadingScreen;
